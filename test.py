764d1dd17d3d04b63d7228725082c930bcc32f98
shakeerdcm <shakeer.dcm@gmail.com>
2026-05-03 21:26:23 +0530

    var,loops

--- test.py
+++ test.py
@@ -40,3 +40,24 @@ data = [1, 2, 3, 4, 5]
 print("Total:", calculate_total(data))
 print("Average:", calculate_average(data))
 print("Transformed:", transform_values(data))
+
+
+calculate_average(data)
+
+def square(number):
+    return number * number
+
+square(125)
+
+# Real-world application example of lists & dictionaries (sorting a list of fruits into bins based on type)✅
+fruit_basket = ['apple', 'banana', 'cherry', 'date', 'eggplant']  
+type_to_bin = {'fruit': [], 'vegetable': []}  
+for fruit in my_list: 
+    if fruit in type_to_bin.keys(): # Checking dictionary keys for item placement (avoid KeyError)
+        type_to_bin[fruit].append(fruit)
+print("Fruits are placed into 'fruit' bin.")  
+for veggie in my_list: 
+    if veggie in type_to_bin.keys(): # Same key-check for vegetable placement (avoid KeyError again)
+        type_to_bin[veggie].append(veggie)
+print("Vegetables are placed into 'vegetable' bin.")  
+
